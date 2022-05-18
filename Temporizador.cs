1a3ed44b99263f74336ba91737b3f74a2ed06c14
EldFranklin <105466304+EldFranklin@users.noreply.github.com>
2022-05-17 23:55:13 -0300

    Create Temporizador.cs
    Temporizador
    recebe um numero que pode ser em segundo ou minuto, e decresce conforme o dado de entrada

---
+++ Temporizador.cs
@@ -0,0 +1,55 @@
+using System.Threading; // BETA, EM ATUALIZACAO
+using System.IO;
+namespace StopWatch{
+    class StopWatch{
+        static void Main(string[]args){
+           Menu();
+        }
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("Para contar em SEGUNDOS, Por exmeplo, digite  >> 10s <<");
+            Console.WriteLine("_____________________");
+            Console.WriteLine("Para contar em MiNUTOS, Por exmeplo digite >> 10m <<");
+            Console.WriteLine("_____________________");
+            Console.WriteLine(" Para sair digite 0.");
+            Console.WriteLine("_____________________");
+            Console.WriteLine("Quantas unidades de tempo quer contar?");
+
+            string tempoDado = Console.ReadLine().ToLower();
+            char tipo = char.Parse(tempoDado.Substring(tempoDado.Length -1 ,1));
+            int tempo = int.Parse(tempoDado.Substring(0 ,tempoDado.Length-1));
+            int mult = 1;
+            if(tipo == 'm'){
+                mult = 60;
+            }
+            if(tempo == 0){
+                System.Environment.Exit(0);
+            }
+           Preparacao(tempo * mult);
+        }
+
+        static void Preparacao(int tempo){
+            Console.Clear();
+            for(int i = 2; i==0; i--){
+                Console.WriteLine($"Começando em{i+1}");
+                Thread.Sleep(1000);
+
+            }
+            Comeco(tempo);
+        }
+        static void Comeco(int time){
+            
+            int contadorTempo = 0;
+            while(contadorTempo != time){
+                Console.Clear();
+                contadorTempo++;
+                Console.WriteLine(contadorTempo);
+                Thread.Sleep(1000);
+
+            }
+            Console.Clear();
+            Console.WriteLine("Contagem acabou");
+            Thread.Sleep(2000);
+        }
+    }
+}
